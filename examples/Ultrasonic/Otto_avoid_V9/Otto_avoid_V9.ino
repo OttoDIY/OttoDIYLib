4435e77d4bd7caadde4f52c6f0777c5e4973f97e
horaceklai <44500643+horaceklai@users.noreply.github.com>
2021-02-19 23:00:40 +0100

    Update Otto_avoid_V9.ino
    Issue 14
    
    Otto now aborts turning when met with an obstacle

--- examples/Ultrasonic/Otto_avoid_V9/Otto_avoid_V9.ino
+++ examples/Ultrasonic/Otto_avoid_V9/Otto_avoid_V9.ino
@@ -12,13 +12,13 @@
 Otto9 Otto;  //This is Otto!
 //---------------------------------------------------------
 //-- First step: Configure the pins where the servos are attached
-/*             -------- 
+/*             --------
               |  O  O  |
               |--------|
   RIGHT LEG 3 |        | LEFT LEG 2
-               -------- 
+               --------
                ||     ||
-RIGHT FOOT 5 |---     ---| LEFT FOOT 4  
+  RIGHT FOOT 5 |---     ---| LEFT FOOT 4
 */
 // SERVO PINs //////////////////////////////////////////////////////////////////////////////
 #define PIN_YL 2 //servo[0]  left leg
@@ -39,22 +39,22 @@ RIGHT FOOT 5 |---     ---| LEFT FOOT 4
 int distance; // variable to store distance read from ultrasonic range finder module
 bool obstacleDetected = false; // logic state for when object detected is at the distance we set
 //-- Movement parameters
-int T=1000;              //Initial duration of movement
-int moveId=0;            //Number of movement
-int moveSize=15;         //Asociated with the height of some movements
+int T = 1000;            //Initial duration of movement
+int moveId = 0;          //Number of movement
+int moveSize = 15;       //Asociated with the height of some movements
 ///////////////////////////////////////////////////////////////////
 //-- Setup ------------------------------------------------------//
 ///////////////////////////////////////////////////////////////////
 void setup() {
   Serial.begin(19200);
   Otto.init(PIN_YL, PIN_YR, PIN_RL, PIN_RR, true, A6, PIN_Buzzer, PIN_Trigger, PIN_Echo); //Set the servo pins and ultrasonic pins and Buzzer pin
-  pinMode(PIN_ASSEMBLY,INPUT_PULLUP); // - Easy assembly pin - LOW is assembly Mode
+  pinMode(PIN_ASSEMBLY, INPUT_PULLUP); // - Easy assembly pin - LOW is assembly Mode
   //Otto wake up!
   Otto.sing(S_connection);// Otto makes a sound
   Otto.home(); // Otto moves to its ready position
   delay(500); // wait for 500 milliseconds to allow Otto to stop
-// if Pin 7 is LOW then place OTTO's servos in home mode to enable easy assembly, 
-// when you have finished assembling Otto, remove the link between pin 7 and GND
+  // if Pin 7 is LOW then place OTTO's servos in home mode to enable easy assembly,
+  // when you have finished assembling Otto, remove the link between pin 7 and GND
   while (digitalRead(PIN_ASSEMBLY) == LOW) {
     Otto.home();// Otto moves to its ready position
     Otto.sing(S_happy_short);   // sing every 5 seconds so we know OTTO is still working
@@ -65,26 +65,33 @@ void setup() {
 //-- Principal Loop ---------------------------------------------//
 ///////////////////////////////////////////////////////////////////
 void loop() {
- if (obstacleDetected) { // if there is an object closer than 15cm then we do the following
-        Otto.sing(S_surprise); // sound a surprise
-        Otto.jump(5, 500); // Otto jumps
-        Otto.sing(S_cuddly); // sound 
-        //Otto takes three steps back
-        for (int i = 0; i < 3; i++) Otto.walk(1, 1300, -1); //repeat  three times the walk back command
-        delay(50);// small delay to allow Otto to settle
-        //Otto turns left 3 steps
-        for (int i = 0; i < 3; i++) { //repeat  three times 
-            Otto.turn(1, 1000, 1); // the walk left command
-            delay(50);// small delay to allow Otto to settle
-            obstacleDetector(); 
-            }  
-          }    
-          else { // if nothing in front then walk forward
-        Otto.walk(1, 1000, 1); //Otto walk straight
-        obstacleDetector(); // call the function to check the ultrasonic range finder for an object closer than 15cm
-        }
-      
-}  
+  if (obstacleDetected) { // if there is an object closer than 15cm then we do the following
+    Otto.sing(S_surprise); // sound a surprise
+    Otto.jump(5, 500); // Otto jumps
+    Otto.sing(S_cuddly); // sound
+    
+    //Otto takes three steps back
+    for (int i = 0; i < 3; i++) Otto.walk(1, 1300, -1); //repeat  three times the walk back command
+    delay(50);// small delay to allow Otto to settle
+    
+    //Otto turns left 3 steps
+    for (int i = 0; i < 3; i++) { //repeat  three times
+      Otto.turn(1, 1000, 1); // the walk left command
+      delay(50);// small delay to allow Otto to settle
+      obstacleDetector();
+
+      // stop turning left if obstacle detected while turning
+      if (obstacleDetected) {
+        break;
+      }
+    }
+  }
+  else { // if nothing in front then walk forward
+    Otto.walk(1, 1000, 1); //Otto walk straight
+    obstacleDetector(); // call the function to check the ultrasonic range finder for an object closer than 15cm
+  }
+
+}
 //////////////////////////////////////////////////////////////////////////////////
 //-- Function to read distance sensor & to actualize obstacleDetected variable //
 /////////////////////////////////////////////////////////////////////////////////
@@ -92,13 +99,13 @@ void obstacleDetector() {
   int distance = Otto.getDistance(); // get the distance from the ultrasonic range finder
   if (distance < 15) obstacleDetected = true; // check to see if this distance is closer than 15cm, true if it is
   else obstacleDetected = false;// false if it is not
-   Serial.print(distance);
+  Serial.print(distance);
   Serial.println("cm");
- if(distance<15){
-          obstacleDetected = true;
-          Serial.println ("Obstacle Detected! Avoid collision");
-        }else{
-          obstacleDetected = false;
-          Serial.println ("No Obstacle detected! - Keep on walking");
-        }
+  if (distance < 15) {
+    obstacleDetected = true;
+    Serial.println ("Obstacle Detected! Avoid collision");
+  } else {
+    obstacleDetected = false;
+    Serial.println ("No Obstacle detected! - Keep on walking");
+  }
 }
